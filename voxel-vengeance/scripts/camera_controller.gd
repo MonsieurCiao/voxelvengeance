49d73f7681d7a7485d12bfb6b86506c79979a4d1
RosdasHH <164037153+RosdasHH@users.noreply.github.com>
2025-05-17 18:29:10 +0200

    Fixed Jitter and sharp shadows

--- voxel-vengeance/scripts/camera_controller.gd
+++ voxel-vengeance/scripts/camera_controller.gd
@@ -9,4 +9,4 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		#rotation.x -= event.relative.y / 1000 * sensitivity
-		rotation.y -= event.relative.x / 1000 * sensitivity
+		rotation.y -= event.relative.x * sensitivity * 0.001
