efd25c77dd48dc3ba5d6ff99a3cc05bf16316a46
MonsieurCiao <luchaokas@gmail.com>
2025-05-19 08:27:12 +0200

    weapon sync (no bullet)

--- voxel-vengeance/scripts/weaponSkript.gd
+++ voxel-vengeance/scripts/weaponSkript.gd
@@ -10,16 +10,18 @@ var bulletSpeed
 var bulletDamage
 
 func _input(event: InputEvent) -> void:
+	if not is_multiplayer_authority(): return
 	if Input.is_key_pressed(KEY_1):
-		summonWeaponWithProperties("pistol", 0.3, Vector3(0,0,-.2), false, 70, 10)
+		summonWeaponWithProperties.rpc("pistol", 0.3, Vector3(0,0,-.2), false, 70, 10)
 	if Input.is_key_pressed(KEY_2):
-		summonWeaponWithProperties("ak47", 0.1, Vector3(0.4,0,0), true, 140, 5)
+		summonWeaponWithProperties.rpc("ak47", 0.1, Vector3(0.4,0,0), true, 140, 5)
 		
 
 func clear_all_children(node: Node) -> void:
 	for child in node.get_children():
 		child.queue_free()
 
+@rpc("call_local")
 func summonWeaponWithProperties(weaponName, cooldown, position, automaticFire, bspeed, damage):
 		bulletSpeed = bspeed
 		bulletDamage = damage
